869af3deeab311b422e27e43b2d3280e2edf9d20
hagarVF <hagar.vois@gmail.com>
2025-01-29 11:11:22 +0200

    change image file path3

--- src/middleware/fileUpload.js
+++ src/middleware/fileUpload.js
@@ -3,7 +3,7 @@ import multer from "multer";
 export const uploadSingleFile = (fieldName) => {
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "images");
+      cb(null, "public/images");
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
